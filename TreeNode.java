daa03f7e4e30691fcabbfa64d2b879eee2997ccd
Nuôi cái bềm <ndhieu026@gmail.com>
2025-06-24 10:35:13 +0700

    Add files via upload

--- TreeNode.java
+++ TreeNode.java
@@ -1,9 +1,10 @@
 
 public class TreeNode {
-	int val;
+	int value;
 	TreeNode left;
 	TreeNode right;
-	public TreeNode(int x) {
-		val = x;
+	public TreeNode(int input) {
+		this.value = input;
 	}
+
 }
